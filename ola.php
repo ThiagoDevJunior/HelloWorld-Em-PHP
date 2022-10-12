7edbefbe40b0e06f369573d9d43cdf073924d5fd
tiago <thiagoangelos2020@gmail.com>
2022-10-12 16:07:34 -0300

    primeiro PHP - hello world

---
+++ ola.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+	<meta charset='utf-8'>
+	<title>Ola PHP</title>
+
+	<style>
+		h2{
+			color: blue;
+		}
+	</style>
+</head>
+<body>
+	<div class='container' align="center">
+		<h1>PRIMEIRO PHP</h1>
+
+		<!--ESTRUTURA 'PHP' -->
+		<?php 
+			echo '<h2><strong>olá mundo - fui Feito Em PHP!</strong></h2>';
+
+		?>
+	</div>
+</body>
+</html>
